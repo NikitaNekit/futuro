672c1eb7e5bbfb2bc9bb0358f03a1d0ed02999f9
MykytaProject <MykytaProject@gmail.com>
2021-11-18 22:05:37 +0100

    privado

--- src/futuro/ed.java
+++ src/futuro/ed.java
@@ -4,7 +4,7 @@ public class ed{
 public static void main(String[]args){
     System.out.println("Estudiate " + args[0] + ": " + veredicto(args[1], args[2]));
         }
-        static String veredicto(String valor1, String valor2){
+        private static String veredicto(String valor1, String valor2){
             if (valor1.equals(valor2)) {
                 return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
             }
