85da489d74e9c8670adc5fe83b827c8ee591547a
Mykyta Ryasny <nikitanekit@gmail.com>
2021-11-18 15:40:54 +0100

    VCS Start

---
+++ src/futuro/ed.java
@@ -0,0 +1,16 @@
+package futuro;
+public class ed{
+
+public static void main(String[]args){
+    System.out.println("Estudiate " + args[0] + ": " + veredicto(args[1], args[2]));
+        }
+        static String veredicto(String valor1, String valor2){
+            if (valor1.equals(valor2)) {
+                return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+            }
+            if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+                return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+            }
+            return "Ideal. Trabajas los contenidos en casa.";
+        }
+}
